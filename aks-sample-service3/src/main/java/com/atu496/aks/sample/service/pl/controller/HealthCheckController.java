ae54f30e74e87ef8dee1a0075989c5842adae46f
Atsushi Morimoto <atsushi.morimoto@cap-net.co.jp>
2020-05-13 17:23:10 +0900

    バックサービスのヘルスチェック用エンドポイントの作成

---
+++ aks-sample-service3/src/main/java/com/atu496/aks/sample/service/pl/controller/HealthCheckController.java
@@ -0,0 +1,19 @@
+package com.atu496.aks.sample.service.pl.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.atu496.aks.sample.service.domain.dto.SampleResponse;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestController
+public class HealthCheckController {
+
+    @GetMapping("/healthcheck")
+    public SampleResponse<String> get(ModelAndView mav) {
+        return new SampleResponse<String>("1", "0", "Success", "HealthCheck OK!!");
+    }
+}
