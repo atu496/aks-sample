0831cfccb7a37d3c0a63cdd6ea387592b3171e28
Atsushi Morimoto <atsushi.morimoto@cap-net.co.jp>
2020-05-13 17:26:54 +0900

    DiscoveryClientを追加してAPIServerからPodの情報を取得できるように

--- aks-sample-app/src/main/java/com/atu496/aks/sample/app/AksSampleAppApplication.java
+++ aks-sample-app/src/main/java/com/atu496/aks/sample/app/AksSampleAppApplication.java
@@ -2,12 +2,14 @@ package com.atu496.aks.sample.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AksSampleAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AksSampleAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(AksSampleAppApplication.class, args);
+    }
 
 }
